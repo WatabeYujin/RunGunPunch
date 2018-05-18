f9874d0b545c2957817232f4eb1e4892361efe2f
Yamaguchi <unaginimono@gmail.com>
2018-05-18 19:01:30 +0900

    障害物エディタのオブジェクトの生成の仕方を変更

--- Yamaguchi/ObjectCreater.cs
+++ Yamaguchi/ObjectCreater.cs
@@ -4,64 +4,66 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 
+/// <summary>
+/// 障害物を生成するエディタ
+/// </summary>
 public class ObjectCreater : EditorWindow {
 
-    private GameObject parent; //障害物の親のオブジェクト
-    private GameObject prefab; //生成するプレファブ
-    //private int numX = 1; //X軸に生成するオブジェクトの数
-    //private int numY = 1; //Y軸に生成するオブジェクトの数
-    private int numZ = 1; //Z軸に生成するオブジェクトの数
-    //private float intervalX = 1; //X軸に生成するオブジェクトの間隔
-    //private float intervalY = 1; //Y軸に生成するオブジェクトの間隔
-    private float intervalZ = 1; //Z軸に生成するオブジェクトの間隔
+    private GameObject parent;                                      //障害物の親のオブジェクト
+    private List<GameObject> prefabs = new List<GameObject>();      //生成するプレファブ
+    private GameObject tObj = null;                                 //listに入れておく変数
+    private int numZ = 1;                                           //Z軸に生成するオブジェクトの数
+    private float intervalZ = 1;                                    //Z軸に生成するオブジェクトの間隔
 
-    private int row = 3; //障害物の行
-    private int column = 2; //障害物の列
-    private Vector3 pos = new Vector3(1,1,0); //障害物の行と列の情報を格納
+    private int row = 3;                                            //障害物の行
+    private int column = 2;                                         //障害物の列
+    private Vector3 pos;                                            //障害物の行と列の情報を格納
 
-    private string outputFileName; //出力するファイルの名前
-    private string dirPath = "Assets/Obstacle/"; //出力するディレクトリのパス
+    private string outputFileName;                                  //出力するファイルの名前
+    private string dirPath = "Assets/Obstacle/";                    //出力するディレクトリのパス
+    private const int range = 3;                                    //障害物の全体の範囲
+    
 
     [MenuItem("Tool/ObjectCreater")]
     static void Init()
     {
-        EditorWindow.GetWindow<ObjectCreater>(true, "ObjectCreater");
+        EditorWindow.GetWindow<ObjectCreater>(true, "ObjectCreater"); //ウィンドウを生成
     }
 
     void OnEnable()
     {
-        if (Selection.gameObjects.Length > 0) parent = Selection.gameObjects[0];
+        if (Selection.gameObjects.Length > 0) parent = Selection.gameObjects[0];    
     }
 
     void OnSelectionChange()
     {
-        if (Selection.gameObjects.Length > 0) prefab = Selection.gameObjects[0];
+        if (Selection.gameObjects.Length > 0) prefabs[0] = Selection.gameObjects[0];
         Repaint();
     }
 
     private void OnGUI()
     {
         parent = EditorGUILayout.ObjectField("Parent", parent, typeof(GameObject),true) as GameObject;
-        prefab = EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), true) as GameObject;
-
-        /*GUILayout.Label("X : ", EditorStyles.boldLabel);
-        numX = int.Parse(EditorGUILayout.TextField("num", numX.ToString()));
-        intervalX = int.Parse(EditorGUILayout.TextField("interval", intervalX.ToString()));
-
-        GUILayout.Label("Y : ", EditorStyles.boldLabel);
-        numY = int.Parse(EditorGUILayout.TextField("num", numY.ToString()));
-        intervalY = int.Parse(EditorGUILayout.TextField("interval", intervalY.ToString()));*/
 
         GUILayout.Label("Z : ", EditorStyles.boldLabel, GUILayout.Width(110));
         numZ = int.Parse(EditorGUILayout.TextField("num", numZ.ToString()));
         intervalZ = int.Parse(EditorGUILayout.TextField("interval", intervalZ.ToString()));
 
+        
+
+        for (int i = 0; i < numZ; i++)
+        {
+            prefabs.Add(tObj);
+            prefabs[i] = EditorGUILayout.ObjectField("Prefab" + i, prefabs[i], typeof(GameObject), true) as GameObject;          
+        }
+        
         EditorGUILayout.Space();
-        GenerationPosition();
+
+        GenerationPosition();       //プレファブを生成する位置を指定
 
         EditorGUILayout.Space();
-        
-        if (GUILayout.Button("Create")) Create();
+
+        if (GUILayout.Button("Create")) Create();       //プレファブを生成
 
         GUILayout.Space(100);
 
@@ -75,12 +77,20 @@ public class ObjectCreater : EditorWindow {
 
             using(new GUILayout.HorizontalScope())
             {
-                if (GUILayout.Button("Save")) SavePrefab();
+                if (GUILayout.Button("Save")) {
+                    
+                    SavePrefab();       //障害物を保存
+
+                }
+                
+                
             }
         }
     }
 
-    //障害物を生成する位置の設定
+    /// <summary>
+    /// 障害物を生成する位置の設定
+    /// </summary>
     private void GenerationPosition()
     {
         GUILayout.Label("Generation position : ", EditorStyles.boldLabel);
@@ -93,8 +103,8 @@ public class ObjectCreater : EditorWindow {
             {
                 if (GUILayout.Button(""))
                 {
-                    pos.x = j * 3;
-                    pos.y = i * 3;
+                    pos.x = j * range;
+                    pos.y = i * range;
                     pos.z = 0;
                     //if (j == 2) pos.y = i * 3 + 1;
                     Debug.Log("posX " + pos.x + "posY " + pos.y + "posZ" + pos.z);
@@ -104,59 +114,22 @@ public class ObjectCreater : EditorWindow {
         }
         EditorGUILayout.EndVertical();
 
-        /*using(new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
-        {
-            for(int i = column; i > 0; i--)
-            {
-                using (new EditorGUILayout.HorizontalScope())
-                {
-                    for (int j = row; j > 0; j--)
-                    {
-                        if (GUILayout.Button(""))
-                        {
-                            pos.x = j*3;
-                            pos.y = i*3;
-                            //if (j == 2) pos.y = i * 3 + 1;
-                        }
-                    }
-                }
-            }
-            
-        }*/
     }
 
-    //オブジェクトの生成
+    /// <summary>
+    /// オブジェクトの生成
+    /// </summary>
     private void Create()
     {
-        if (prefab == null) return;
+        if (prefabs[0] == null) return;
 
         int count = 0;
 
-        /*pos.x = -(numX - 1) * intervalX / 2;
-        for (int x = 0; x < numX; x++)
-        {
-            pos.y = -(numY - 1) * intervalY / 2;
-            for (int y = 0; y < numY; y++)
-            {
-                pos.z = -(numZ - 1) * intervalZ / 2;
-                for (int z = 0; z < numZ; z++)
-                {
-                    GameObject obj = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
-                    obj.name = prefab.name + count++;
-                    if (parent) obj.transform.parent = parent.transform;
-                    Undo.RegisterCreatedObjectUndo(obj, "Object Creater");
-
-                    pos.z += intervalZ;
-                }
-                pos.y += intervalY;
-            }
-            pos.x += intervalX;
-        }*/
         pos.z = -(numZ - 1) * intervalZ / 2;
-        for (int z = 0; z < numZ; z++)
+        for (int i = 0; i < numZ; i++)
         {
-            GameObject obj = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
-            obj.name = prefab.name + count++;
+            GameObject obj = Instantiate(prefabs[i], pos, Quaternion.identity) as GameObject;
+            obj.name = prefabs[i].name + count++;
             if (parent) obj.transform.parent = parent.transform;
             Undo.RegisterCreatedObjectUndo(obj, "Object Creater");
 
@@ -166,9 +139,11 @@ public class ObjectCreater : EditorWindow {
         
     }
 
-    //プレファブをセーブ
+    /// <summary>
+    /// プレファブをセーブ
+    /// </summary>
     private void SavePrefab()
-    {       
+    {
         if (!Directory.Exists(dirPath))
         {
             //prefab保存用のフォルダがなければ作成する
