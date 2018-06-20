5b063d9e7a100f729f58d3ba6bd9afd38cd966ab
YujinWatabe <15c1020043wy@edu.tech.ac.jp>
2018-06-20 19:38:25 +0900

    一定間隔でスポーンさせる処理と発射する弾丸の弾道のスクリプトのコミット

---
+++ WatabeYujin/EnemySpawn.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+    [SerializeField]
+    private List<GameObject> spawnPrefub;       //スポーンを行う順にPrefubを入れる
+    [SerializeField]
+    private float spawnInterval = 100;          //スポーンを行う間隔
+    [SerializeField]
+    private Transform spawnPosition;            //prefubをスポーンさせる位置
+
+    private float timeCount;                    //経過時間をカウント、スポーン毎に0になる。
+    private bool isGameActive = true;           //ゲームが動いているか
+    private int spawnCount = 0;                 //スポーンを行った回数
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        TimeCount();
+        if (!SpawnTimeCheck(timeCount)) return;
+        SpawnEvent();
+    }
+
+    /// <summary>
+    /// deltaTimeによる時間のカウントを行う
+    /// </summary>
+    void TimeCount()
+    {
+        if (!isGameActive) return;
+        timeCount += Time.deltaTime;
+    }
+
+    /// <summary>
+    /// スポーンを行うタイミングのチェックを行う
+    /// </summary>
+    /// <param name="time">調べる時間</param>
+    /// <returns>スポーンできる場合trueを返し,不可能な場合falseを返す</returns>
+    bool SpawnTimeCheck(float time)
+    {
+        if(spawnInterval >= time)
+        {
+            timeCount = 0;
+            return true;
+        }
+        else return false;
+    }
+
+    /// <summary>
+    /// 実際にprefubをスポーンさせる
+    /// </summary>
+    /// <returns>スポーンできた場合true,できなかった場合falseを返す</returns>
+    bool SpawnEvent()
+    {
+        if (spawnCount > spawnPrefub.Count) return false;
+        if (!spawnPrefub[spawnCount]) return false;
+        
+        GameObject spawnObj = Instantiate(spawnPrefub[spawnCount]);
+        spawnObj.transform.position = spawnPosition.position;
+        spawnCount++;
+
+        return true;
+    }
+}
---
+++ WatabeYujin/HomingBullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingBullet : MonoBehaviour {
+    [SerializeField]
+    Transform homingTransform;
+    [SerializeField]
+    private bool isMove;
+
+    private const float speed = 10;
+    Rigidbody thisRigidbody;
+	[SerializeField]
+    private float targetDistance;
+	Vector3 centerPos;
+
+	void Start () {
+        thisRigidbody = GetComponent<Rigidbody>();
+		TargetTransformSet (homingTransform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BulletMove();
+	}
+
+    void BulletMove()
+    {
+        if (!isMove) return;
+        thisRigidbody.velocity = transform.forward *speed;
+	}
+    public void TargetTransformSet(Transform targetTransform)
+    {
+        homingTransform = targetTransform;
+        isMove = true;
+    }
+
+    public bool IsMove
+    {
+        set
+        {
+            isMove = value;
+        }
+        get
+        {
+            return isMove;
+        }
+    }
+}
